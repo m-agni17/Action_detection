1d9364d296a28a430071a27903da1d24484b818f
Agni Prasanth <72970324+m-agni17@users.noreply.github.com>
2022-09-11 23:10:08 +0530

    Update st.py

--- st.py
+++ st.py
@@ -1,54 +1,69 @@
-import streamlit as st
 import cv2
-import numpy as np
+import streamlit as st
 import time
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras. preprocessing import image
-model=tf.keras.models.load_model("DeepVisionModel.h5")
-cap = cv2.VideoCapture(0)
-FRAME=st.image([])
+
+
+st.title("SIGNED OR UNSIGNED")
+run = st.checkbox('Click to Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+model2 = tf.keras.models.load_model('DeepVisionModel.h5')
 font = cv2.FONT_HERSHEY_SIMPLEX
-org = (0, 30)
-fontScale = 0.5
+# org
+org = (80, 224)
+
+fontScale = 1
+
 color = (255, 0, 0)
-thickness = 2
-        
 
-        
-        
-# loop runs if capturing has been initialized.
-while (True):
-    # reads frames from a camera
-    # ret checks return at each frame
-    _, frame = cap.read()
-    time.sleep(0.1)
-    _, frame1 = cap.read()
+thickness = 3
+while run:
+    return_value, frame1 = camera.read()
+    FRAME_WINDOW.image(frame1)
+    time.sleep(0.001)
 
+    return_value, frame2 = camera.read()
+    FRAME_WINDOW.image(frame2)
 
+    time.sleep(0.001)
+else:
+    st.markdown("<h4 style='text-align: center; color: gray;'>Bye..</h4>", unsafe_allow_html=True)
 
-    image_1_resize = cv2.resize(frame, (256, 256))
-    img = cv2.cvtColor(image_1_resize, cv2.COLOR_BGR2GRAY)
-    imgstack = np.dstack([img] * 3)
-
-    image_1_resize1 = cv2.resize(frame1, (256, 256))
-    img1 = cv2.cvtColor(image_1_resize1, cv2.COLOR_BGR2GRAY)
-    imgstack1=np.dstack([img1]*3)
-    absdiff = cv2.absdiff(imgstack, imgstack1)
-    FRAME.image(absdiff)
-    abs1=np.expand_dims(absdiff,axis=0)
     
-    val = model.predict(abs1)
+
+
+    cv2.imwrite("image1.png", frame1)
+    image1 = cv2.imread("image1.png",0)
+    #image1 = cv2.cvtColor(frame1, cv2.IMREAD_COLOR)
+    #image1 = cv2.imdecode(frame1, cv2.IMREAD_COLOR)
+    #image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image1 = cv2.resize(image1,(256,256))
+    image1 = np.dstack([image1]*3)
+    cv2.imwrite("image2.png", frame2)
+    image2 = cv2.imread("image2.png",0)
+    #image2 = cv2.cvtColor(frame2, cv2.IMREAD_COLOR)
+    #image2 = cv2.imdecode(frame2, cv2.IMREAD_COLOR)
+    #image2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    image2 = cv2.resize(image2,(256,256))
+    image2 = np.dstack([image2]*3)
+
+
+
+
+    absdiff = cv2.absdiff(image1,image2)
+    absdiff1 = np.expand_dims(absdiff, axis = 0)
+    val = model2.predict(absdiff1)
     if val == 0:
-        absdiff = cv2.putText(absdiff, 'Unsigned', org, font, 
+         absdiff = cv2.putText(absdiff, 'Signed', org, font, 
                    fontScale, color, thickness, cv2.LINE_AA)
-        FRAME.image(absdiff)
-
+         FRAME_WINDOW.image(absdiff)
     else:
-        absdiff = cv2.putText(absdiff, 'Signed', org, font, 
+         absdiff = cv2.putText(absdiff, 'Unsigned', org, font, 
                    fontScale, color, thickness, cv2.LINE_AA)
-        FRAME.image(absdiff)
-    
+         FRAME_WINDOW.image(absdiff)
+
 
 
 
